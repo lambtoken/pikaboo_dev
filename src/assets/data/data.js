1b1478d496b2c3db08ad10a7f9ba924bde52425c
David <anru1996@gmail.com>
2023-10-04 18:42:26 +0200

    data

---
+++ src/assets/data/data.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+const PortfolioData = [
+  {
+    title: "Slideas",
+    summary:
+      "Designed a sleek one-page site highlighting our client's presentation skills. Built on WordPress with a custom theme, the site elegantly showcases their expertise. Visitors can now explore captivating storytelling and design, enhancing the impact of pitches and presentations.",
+    technologies: ["WordPress"],
+    img: "",
+  },
+  {
+    title: "VistaHomes",
+    summary:
+      "Crafted a unique WordPress site for Vista Homes, emphasizing their real estate expertise. Using a custom theme, we showcased their quality, client-centered approach, and innovative designs in a straightforward manner.",
+    technologies: ["WordPress"],
+    img: "",
+  },
+  {
+    title: "AfroCentric Records",
+    summary:
+      "Crafted a sophisticated WordPress platform for DJ Cory Centric, integrating artist-specific pages, album databases, and event archives. Leveraging intricate data structures, the site seamlessly navigates his musical journey, showcasing his bio, Centric Music label, and Home of Afro House events with precision",
+    technologies: ["WordPress"],
+    img: "",
+  },
+  {
+    title: "Krnjevac",
+    summary:
+      "Crafted an efficient e-commerce site for Krnjevac, Serbia's top honey distributor. Powered by WordPress and WooCommerce, the platform showcases their range of 100% natural honey products, ensuring seamless online shopping. Emphasizing quality and authenticity, it delivers a streamlined customer experience",
+    technologies: ["WordPress", "E-Commerce"],
+    img: "",
+  },
+  {
+    title: "Body By Margie",
+    summary:
+      "Designed a tailored WordPress site, 'Body By Margie', for a Canadian fitness and diet coach. Customized with subscription features, the platform offers seamless interactions for clients, enhancing their fitness journey. A user-friendly hub empowering healthy lifestyles.",
+    technologies: ["WordPress", "E-Commerce"],
+    img: "",
+  },
+  {
+    title: "Streetkopi",
+    summary:
+      "Built a professional website for 'Streetkopi,' integrating a local print shop and bookstore. Utilizing WordPress, the platform offers streamlined ordering and essential information.",
+    technologies: ["WordPress", "E-Commerce"],
+    img: "",
+  },
+  {
+    title: "Creddit",
+    summary:
+      "Developed 'Creddit,' a Reddit-inspired forum platform, as a MERN stack exercise. With user-friendly features mirroring Reddit, Creddit enables seamless interactions, including posts, comments, users, and subreddits.",
+    technologies: ["React", "Node.js"],
+    img: "",
+  },
+  {
+    title: "Portfolio",
+    summary:
+      "Presenting this very website - A journey in one space. A React-powered portfolio showcasing our diverse projects, from e-commerce and forums to fitness coaching websites. A dynamic snapshot of our expertise and innovation, all in one place.",
+    technologies: ["React"],
+    img: "",
+  },
+];
+
+const AboutData = `We're David, Milivoje, and Borivoje, a team passionate about crafting incredible online experiences.
+
+Dynamic online stores, WordPress and Drupal websites, powerful apps using React/Node.js - We do it all
+
+Our websites don't just look good; they work great too. We follow the best methods to make sure your site is both attractive and easy to use.
+Simply put. We're all about quality. 
+
+Whether you're a small business starting out or a big company looking to change things up, we've got you covered. Our services are personalized and will fit your needs perfectly.
+
+Let's work together to bring your digital dreams to life. Get in touch, and let's make your online presence shine!`;
+
+export { PortfolioData, AboutData };
