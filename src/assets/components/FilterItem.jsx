c907eca5f8d34d2828f2c409a9b125bb9497a05b
milivojejankovic <fiendish@DESKTOP-EDQ019F>
2023-10-14 13:48:54 +0200

    lazyloading

--- src/assets/components/FilterItem.jsx
+++ src/assets/components/FilterItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 import Categories from './Categories';
 
 import "./FilterItem.css";
@@ -7,8 +9,20 @@ import "./FilterItem.css";
 function FilterItem(props) {
   return (
     <div className={`portfolio-item ${props.className}`} onClick={props.onClick} >
+      <lazySizes className="img"></lazySizes>
       <span className='portfolio-title'> {props.portfolioItem.title}</span>
-      <img src={props.portfolioItem.img}/>
+      <LazyLoadImage
+        alt="thumbnail"
+        effect="blur"
+        src={`./images/${props.portfolioItem.title}.png`}
+        placeholderSrc={`./images/${props.portfolioItem.title}_low.jpeg`}
+      />
+      {/* <img
+	      alt="thumbnail"
+	      src={`./images/${props.portfolioItem.title}_low`}
+	      data-src={`./images/${props.portfolioItem.title}`}
+	      className="lazyload"
+      /> */}
       <Categories categories={props.portfolioItem.technologies}/>
     </div>
   )
